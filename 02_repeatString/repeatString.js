53f73e9370ee1858c3bcd1d0b58de05423ad64c6
Avathanshu <avathanshu@gmail.com>
2024-06-16 20:18:17 +1200

    JS Exercise 2 completion

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let out = "";
+    for (let i = 0; i < num; i++) {
+        out = out + string;
+    }
+    if (Math.sign(num) == -1) {
+        return "ERROR";
+    }
+    else {
+        return out;
+    }
 };
 
 // Do not edit below this line
