b48e80921aaec2f5e25c76762226a605977cbbee
Avathanshu <avathanshu@gmail.com>
2024-06-17 15:34:59 +1200

    JS Exercise 3 completion

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = []
+    let stringReversed = ""
+    const stringLength = string.length
+    for (i = 0; i < stringLength; i++) {
+        stringArray.unshift(`${string[i]}`)
+    }
+    for (i = 0; i < stringArray.length; i++) {
+        stringReversed = stringReversed + stringArray[i]
+    }
+    return(stringReversed)
+}
 
-};
+console.log(reverseString("Hello there"))
 
 // Do not edit below this line
 module.exports = reverseString;
