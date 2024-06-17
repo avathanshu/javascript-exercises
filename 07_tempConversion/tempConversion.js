1d3e47b39e962908ffad6737454683b8b456670d
Avathanshu <avathanshu@gmail.com>
2024-06-17 18:50:56 +1200

    JS Exercise 7 completion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let temp = (fahrenheit - 32) * (5/9);
+  if (Number.isInteger(temp)) {
+    return temp;
+  }
+  else {
+    temp = temp.toFixed(1);
+    return parseFloat(temp);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let temp = (celsius * (9/5)) + 32;
+  if (Number.isInteger(temp)) {
+    return temp;
+  }
+  else {
+    temp = temp.toFixed(1);
+    return parseFloat(temp);
+  }
 };
 
 // Do not edit below this line
