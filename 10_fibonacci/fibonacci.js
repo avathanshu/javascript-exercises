2a9f20ad2aa41ef0723b4e3c5bd644453c4ec6a5
Avathanshu <avathanshu@gmail.com>
2024-06-20 16:52:08 +1200

    fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const arr = [1]
+    for (let i = 1; i < 5; i++) {
+        let series = arr.reduce((total, currentvalue) => , 0)
+        arr.push(series)
+        console.log(arr)
+    }
+    
 };
+
+console.log(fibonacci(2))
 
 // Do not edit below this line
 module.exports = fibonacci;
